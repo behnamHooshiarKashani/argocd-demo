662e4757b1b5955fe1318b2c2e852e882d69b0d1
behnam <behnam@ng-voice.com>
2021-09-16 16:09:12 +0200

    add whereabouts charts

--- packagingAndUploadingHelmCharts.sh
+++ packagingAndUploadingHelmCharts.sh
@@ -2,7 +2,7 @@
 sourcePath="/Users/behnam/ngvoice/repos/bitbucket/helm-charts/"
 destPath="/Users/behnam/ngvoice/repos/k8s/github-helm-repo/argocd-demo/charts"
 
-for chart in b2bua cscf-configuration-controller cscf-controller icscf local-path-provisioner mysql powerdns-recursor powerdns-server scscf srvcc test
+for chart in b2bua cscf-configuration-controller cscf-controller icscf local-path-provisioner mysql powerdns-recursor powerdns-server scscf srvcc test whereabouts
 do
         echo "$chart"
         helm package $sourcePath$chart -d $destPath
