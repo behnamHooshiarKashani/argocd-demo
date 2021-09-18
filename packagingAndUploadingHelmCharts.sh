515f3850e128f67364cbd14845161e1181328864
behnam <behnam@ng-voice.com>
2021-09-18 09:27:51 +0200

    add powerdns recursor charts

--- packagingAndUploadingHelmCharts.sh
+++ packagingAndUploadingHelmCharts.sh
@@ -2,6 +2,9 @@
 sourcePath="/Users/behnam/ngvoice/repos/bitbucket/helm-charts/"
 destPath="/Users/behnam/ngvoice/repos/k8s/github-helm-repo/argocd-demo/charts"
 
+helm dependency update $sourcePath/powerdns-server
+helm dependency update $sourcePath/powerdns-recursor
+
 for chart in b2bua cscf-configuration-controller cscf-controller icscf local-path-provisioner mysql powerdns-recursor scscf srvcc test whereabouts
 do
         echo "$chart"
